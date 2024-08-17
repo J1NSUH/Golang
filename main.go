28184e100bdac7e8652374d02b5cc7eacaf42151
j1nsuh <jinsuh0520@gmail.com>
2024-08-17 13:07:20 +0900

    1-0

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"Golang/something" // something 패키지를 가져옵니다
+	"fmt"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello world")
+	something.SayHello() // something 패키지의 함수를 호출합니다
 }
