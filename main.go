20928439dac53de7523ea563d0a73b6447672ac5
j1nsuh <jinsuh0520@gmail.com>
2024-08-30 13:56:43 +0900

    1-4

--- main.go
+++ main.go
@@ -9,8 +9,17 @@ func multiply(a,b int) int {
 	return a*b
 }
 
-func lenAndUpper(name string)(int, string){
+func lenAndUpper_1_3(name string)(int, string){
 	return len(name),strings.ToUpper(name)
+}
+
+
+func lenAndUpper_1_4(name string)(length int,uppercase string){
+	// defer는 function이 값을 return하고 나면 실행됨
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func repeatMe(words ...string){
@@ -19,7 +28,10 @@ func repeatMe(words ...string){
 
 func main() {
 	fmt.Println(multiply(2,2))
-	totalLenght, _ :=lenAndUpper("nico")
-	fmt.Println(totalLenght)
+	totalLenght_1_3, _ :=lenAndUpper_1_3("nico")
+	fmt.Println(totalLenght_1_3)
 	repeatMe("jinsuh","nico","Lynn","test")
+	fmt.Println("1-4")
+	totalLenght_1_4, _ :=lenAndUpper_1_4("nico")
+	fmt.Println(totalLenght_1_4)
 }
